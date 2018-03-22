6969bd5ede47a71997e2906f844e25890942246e
Pavel Vyskocil <vyskocilpavel@muni.cz>
2018-03-22 10:45:31 +0100

    Changed the disco-template to use CESNET's WAYF

--- themes/cesnet/perun/disco-tpl.php
+++ themes/cesnet/perun/disco-tpl.php
@@ -9,177 +9,19 @@
 
 $this->data['jquery'] = array('core' => TRUE, 'ui' => TRUE, 'css' => TRUE);
 
-$this->data['head'] = '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML\Module::getModuleUrl('discopower/style.css')  . '" />';
-$this->data['head'] .= '<link rel="stylesheet" media="screen" type="text/css" href="' . SimpleSAML\Module::getModuleUrl('cesnet/res/css/disco.css')  . '" />';
-
-$this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML\Module::getModuleUrl('discopower/js/jquery.livesearch.js')  . '"></script>';
-$this->data['head'] .= '<script type="text/javascript" src="' . SimpleSAML\Module::getModuleUrl('discopower/js/suggest.js')  . '"></script>';
-
-$this->data['head'] .= searchScript();
-
-
 
 $this->includeAtTemplateBase('includes/header.php');
 
+$url = $this->getContinueUrlWithoutIdPEntityId();
 
-
-if (!empty($this->getPreferredIdp())) {
-
-	echo '<p class="descriptionp">' . $this->t('{cesnet:einfra:disco_previous_selection}') . '<EXTERNAL_FRAGMENT></EXTERNAL_FRAGMENT></p>';
-	echo '<div class="metalist list-group">';
-	echo showEntry($this, $this->getPreferredIdp(), true);
-	echo '</div>';
-
-
-	echo getOr($this);
-}
-
-
-
-
-echo '<div class="row">';
-foreach ($this->getIdps('social') AS $idpentry) {
-
-	echo '<div class="col-md-4">';
-	echo '<div class="metalist list-group">';
-	echo showEntry($this, $idpentry, false);
-	echo '</div>';
-	echo '</div>';
-
-}
-echo '</div>';
-
-
-
-echo getOr($this);
-
-
-
-echo '<p class="descriptionp">';
-echo $this->t('{cesnet:einfra:disco_institutional_account}');
-echo '</p>';
-
-echo '<div class="inlinesearch">';
-echo '	<form id="idpselectform" action="?" method="get">
-			<input class="inlinesearchf form-control input-lg" placeholder="' . $this->t('{cesnet:einfra:name_of_institution}') . '" 
-			type="text" value="" name="query" id="query" autofocus oninput="document.getElementById(\'list\').style.display=\'block\';"/>
-		</form>';
-echo '</div>';
-
-
-echo '<div class="metalist list-group" id="list">';
-foreach ($this->getIdps() AS $idpentry) {
-	echo showEntry($this, $idpentry, false);
-}
-echo '</div>';
-
-
-echo '<br>';
-echo '<br>';
-
-
-echo '<div class="no-idp-found alert alert-info">';
-if ($this->isOriginalSpNonFilteringIdPs()) {
-	echo $this->t('{cesnet:einfra:find_institution_contact}') . '<a href="mailto:aai-contact@cesnet.cz?subject=Request%20for%20adding%20new%20IdP">aai-contact@cesnet.cz</a>';
+if (isset($this->data['originalsp']['filter'])){
+	$filter = $this->data['originalsp']['filter'] ;
 } else {
-	echo $this->t('{cesnet:einfra:find_institution_extended}') . '<a class="btn btn-primary" href="https://login.cesnet.cz/add-institution/"> ' . $this->t('{cesnet:einfra:add_institution}') . ' </a>';
+	$filter = 'eyJ2ZXIiOiIyIiwiYWxsb3dGZWVkcyI6eyJlZHVHQUlOIjp7fX19';
 }
 
+header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $filter);
 
-echo '</div>';
-
-
-
-
+exit;
 
 $this->includeAtTemplateBase('includes/footer.php');
-
-
-
-
-
-
-
-
-function searchScript() {
-
-	$script = '<script type="text/javascript">
-
-	$(document).ready(function() { 
-		$("#query").liveUpdate("#list");
-	});
-	
-	</script>';
-
-	return $script;
-}
-
-/**
- * @param sspmod_perun_DiscoTemplate $t
- * @param array $metadata
- * @param bool $favourite
- * @return string html
- */
-function showEntry($t, $metadata, $favourite = false) {
-
-	if (isset($metadata['tags']) && in_array('social', $metadata['tags'])) {
-		return showEntrySocial($t, $metadata);
-	}
-
-	$extra = ($favourite ? ' favourite' : '');
-	$html = '<a class="metaentry' . $extra . ' list-group-item" href="' . $t->getContinueUrl($metadata['entityid']) . '">';
-
-	$html .= '<strong>' . $t->getTranslatedEntityName($metadata) . '</strong>';
-
-	$html .= showIcon($metadata);
-
-	$html .= '</a>';
-
-	return $html;
-}
-
-/**
- * @param sspmod_perun_DiscoTemplate $t
- * @param array $metadata
- * @return string html
- */
-function showEntrySocial($t, $metadata) {
-
-	$bck = 'white';
-	if (!empty($metadata['color'])) {
-		$bck = $metadata['color'];
-	}
-
-	$html = '<a class="btn btn-block social" href="' . $t->getContinueUrl($metadata['entityid'])  . '" style="background: '. $bck .'">';
-
-	$html .= '<img src="' . $metadata['icon'] . '">';
-
-	$html .= '<strong> '. $t->t('{cesnet:einfra:sign_with}') . $t->getTranslatedEntityName($metadata) . '</strong>';
-
-	$html .= '</a>';
-
-	return $html;
-}
-
-
-function showIcon($metadata) {
-	$html = '';
-	// Logos are turned off, because they are loaded via URL from IdP. Some IdPs have bad configuration, so it breaks the WAYF.
-
-	/*if (isset($metadata['UIInfo']['Logo'][0]['url'])) {
-		$html .= '<img src="' . htmlspecialchars(\SimpleSAML\Utils\HTTP::resolveURL($metadata['UIInfo']['Logo'][0]['url'])) . '" class="idp-logo">';
-	} else if (isset($metadata['icon'])) {
-		$html .= '<img src="' . htmlspecialchars(\SimpleSAML\Utils\HTTP::resolveURL($metadata['icon'])) . '" class="idp-logo">';
-	}*/
-
-	return $html;
-}
-
-
-function getOr($t) {
-	$or  = '<div class="hrline">';
-	$or .= '	<span>' . $t->t('{cesnet:einfra:or}') . '</span>';
-	$or .= '</div>';
-	return $or;
-}
-
