5b06f5659c739090cc8505499f0ac28413a864da
Pavel Vyskocil <vyskocilpavel@muni.cz>
2018-04-27 12:10:52 +0200

    Change the template for discoService
    * Now the discoService get the attribute AuthnContextClassRef attribute from SAML2 Request.
    * SP can send the filter/efilter value or entityId of the IdP
    * If SP didn't send the filter/efilter value, we get filter from metadata or use the default filter(All eduGain + SocialIdP) if filter not set in in SP metadata

--- themes/cesnet/perun/disco-tpl.php
+++ themes/cesnet/perun/disco-tpl.php
@@ -7,21 +7,52 @@
  * @var sspmod_perun_DiscoTemplate $this
  */
 
-$this->data['jquery'] = array('core' => TRUE, 'ui' => TRUE, 'css' => TRUE);
+const URN_CESNET_PROXYIDP_FILTER = "urn:cesnet:proxyidp:filter:";
+const URN_CESNET_PROXYIDP_EFILTER = "urn:cesnet:proxyidp:efilter:";
+const URN_CESNET_PROXYIDP_IDPENTITYID = "urn:cesnet:proxyidp:idpentityid:";
 
+$filter = null;
+$efilter = null;
+$idpEntityId = null;
+$authContextClassRef = $this->data['AuthnContextClassRef'];
+$defaultFilter = 'eyJ2ZXIiOiIyIiwiYWxsb3dGZWVkcyI6eyJlZHVHQUlOIjp7fX19';
+
+$this->data['jquery'] = array('core' => TRUE, 'ui' => TRUE, 'css' => TRUE);
 
 $this->includeAtTemplateBase('includes/header.php');
 
-$url = $this->getContinueUrlWithoutIdPEntityId();
+foreach ($authContextClassRef as $value) {
 
-if (isset($this->data['originalsp']['filter'])){
-	$filter = $this->data['originalsp']['filter'] ;
-} else {
-	$filter = 'eyJ2ZXIiOiIyIiwiYWxsb3dGZWVkcyI6eyJlZHVHQUlOIjp7fX19';
+	if (substr($value, 0, strlen(URN_CESNET_PROXYIDP_FILTER)) === URN_CESNET_PROXYIDP_FILTER) {
+		$filter = substr($value, strlen(URN_CESNET_PROXYIDP_FILTER), strlen($value));
+	} elseif (substr($value, 0, strlen(URN_CESNET_PROXYIDP_EFILTER)) === URN_CESNET_PROXYIDP_EFILTER) {
+		$efilter = substr($value, strlen(URN_CESNET_PROXYIDP_EFILTER), strlen($value));
+	} elseif (substr($value, 0, strlen(URN_CESNET_PROXYIDP_IDPENTITYID)) === URN_CESNET_PROXYIDP_IDPENTITYID) {
+		$idpEntityId = substr($value, strlen(URN_CESNET_PROXYIDP_IDPENTITYID), strlen($value));
+	}
 }
 
-header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $filter);
+if ($idpEntityId != null) {
+	$url = $this->getContinueUrl($idpEntityId);
 
-exit;
+	SimpleSAML\Utils\HTTP::redirectTrustedURL($url);
+	exit;
+} else {
+	$url = $this->getContinueUrlWithoutIdPEntityId();
+	if ($efilter != null) {
+		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&efilter=' . $efilter);
+		exit;
+	} elseif ($filter != null) {
+		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $filter);
+		exit;
+	}elseif (isset($this->data['originalsp']['filter'])) {
+		$filter = $this->data['originalsp']['filter'];
+		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $filter);
+		exit;
+	} else {
+		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $defaultFilter);
+		exit;
+	}
+}
 
 $this->includeAtTemplateBase('includes/footer.php');
