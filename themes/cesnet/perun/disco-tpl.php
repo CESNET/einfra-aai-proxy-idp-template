ea0d36565b6bc541787fb7677c82e33b48913073
Pavel Vyskočil <vyskocilpavel@muni.cz>
2018-05-22 12:15:34 +0200

    Added options to set defaultFilter and defaultEfilter
    * Added option to set defaultFilter and defaultEfilter in saml20-idp-hosted metadata, so now we can update only metadata with filter change

--- themes/cesnet/perun/disco-tpl.php
+++ themes/cesnet/perun/disco-tpl.php
@@ -11,24 +11,41 @@ const URN_CESNET_PROXYIDP_FILTER = "urn:cesnet:proxyidp:filter:";
 const URN_CESNET_PROXYIDP_EFILTER = "urn:cesnet:proxyidp:efilter:";
 const URN_CESNET_PROXYIDP_IDPENTITYID = "urn:cesnet:proxyidp:idpentityid:";
 
+$metadata = SimpleSAML_Metadata_MetaDataStorageHandler::getMetadataHandler();
+$idpmeta = $metadata->getMetaData('https://login.cesnet.cz/idp/', 'saml20-idp-hosted');
+
 $filter = null;
 $efilter = null;
 $idpEntityId = null;
-$authContextClassRef = $this->data['AuthnContextClassRef'];
-$defaultFilter = 'eyJ2ZXIiOiIyIiwiYWxsb3dGZWVkcyI6eyJlZHVHQUlOIjp7fX19';
+$authContextClassRef = null;
+$defaultFilter = null;
+$defaultEFilter = null;
+
+if (isset($this->data['AuthnContextClassRef'])) {
+	$authContextClassRef = $this->data['AuthnContextClassRef'];
+}
+
+if (isset($idpmeta['defaultFilter'])) {
+	SimpleSAML\Logger::debug(print_r($idpmeta['defaultFilter'], true));
+	$defaultFilter = $idpmeta['defaultFilter'];
+}
+if (isset($idpmeta['defaultEFilter'])) {
+	$defaultEFilter = $idpmeta['defaultEFilter'];
+}
+
 
 $this->data['jquery'] = array('core' => TRUE, 'ui' => TRUE, 'css' => TRUE);
-
 $this->includeAtTemplateBase('includes/header.php');
 
-foreach ($authContextClassRef as $value) {
-
-	if (substr($value, 0, strlen(URN_CESNET_PROXYIDP_FILTER)) === URN_CESNET_PROXYIDP_FILTER) {
-		$filter = substr($value, strlen(URN_CESNET_PROXYIDP_FILTER), strlen($value));
-	} elseif (substr($value, 0, strlen(URN_CESNET_PROXYIDP_EFILTER)) === URN_CESNET_PROXYIDP_EFILTER) {
-		$efilter = substr($value, strlen(URN_CESNET_PROXYIDP_EFILTER), strlen($value));
-	} elseif (substr($value, 0, strlen(URN_CESNET_PROXYIDP_IDPENTITYID)) === URN_CESNET_PROXYIDP_IDPENTITYID) {
-		$idpEntityId = substr($value, strlen(URN_CESNET_PROXYIDP_IDPENTITYID), strlen($value));
+if ($authContextClassRef != null) {
+	foreach ($authContextClassRef as $value) {
+		if (substr($value, 0, strlen(URN_CESNET_PROXYIDP_FILTER)) === URN_CESNET_PROXYIDP_FILTER) {
+			$filter = substr($value, strlen(URN_CESNET_PROXYIDP_FILTER), strlen($value));
+		} elseif (substr($value, 0, strlen(URN_CESNET_PROXYIDP_EFILTER)) === URN_CESNET_PROXYIDP_EFILTER) {
+			$efilter = substr($value, strlen(URN_CESNET_PROXYIDP_EFILTER), strlen($value));
+		} elseif (substr($value, 0, strlen(URN_CESNET_PROXYIDP_IDPENTITYID)) === URN_CESNET_PROXYIDP_IDPENTITYID) {
+			$idpEntityId = substr($value, strlen(URN_CESNET_PROXYIDP_IDPENTITYID), strlen($value));
+		}
 	}
 }
 
@@ -45,13 +62,22 @@ if ($idpEntityId != null) {
 	} elseif ($filter != null) {
 		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $filter);
 		exit;
-	}elseif (isset($this->data['originalsp']['filter'])) {
+	} elseif (isset($this->data['originalsp']['efilter'])) {
+		$efilter = $this->data['originalsp']['efilter'];
+		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&efilter=' . $efilter);
+		exit;
+	} elseif (isset($this->data['originalsp']['filter'])) {
 		$filter = $this->data['originalsp']['filter'];
 		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $filter);
 		exit;
-	} else {
+	} elseif ($defaultEFilter != null) {
+		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&efilter=' . $defaultEFilter);
+		exit;
+	} elseif ($defaultFilter != null) {
 		header('Location: https://ds.eduid.cz/wayf.php' . $url . '&filter=' . $defaultFilter);
 		exit;
+	} else {
+		throw new SimpleSAML_Error_Exception("cesnet:disco-tpl: Filter did not set. ");
 	}
 }
 
